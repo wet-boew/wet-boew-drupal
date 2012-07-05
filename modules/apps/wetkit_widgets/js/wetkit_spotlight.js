b9157f5f76b2a5c2304d3c0898e8f935cd5de7a3
sylus1984 <sylus1984@172.20.10.2>
2012-07-04 23:16:22 -0400

    Fix for problems with Saving Node Repeatedly (Constraint Error)

--- modules/apps/wetkit_widgets/js/wetkit_spotlight.js
+++ modules/apps/wetkit_widgets/js/wetkit_spotlight.js
@@ -5,7 +5,8 @@
  Drupal.behaviors.wetkitSpotlight = {
    attach: function (context, settings) {
      if ($('.field-name-field-basic-spotlight-items').length) {
-       $('.field-name-field-basic-spotlight-items').tabs().tabs("rotate", 4000, true);
+     	var rotation_time = Drupal.settings.spotlight_settings.rotation_time;
+       $('.field-name-field-basic-spotlight-items').tabs().tabs("rotate", rotation_time, true);
        // $('.field-name-field-basic-spotlight-items').css('height', $('.field-name-field-basic-spotlight-items').height());
        // $('.field-name-field-basic-spotlight-items').css('overflow', 'hidden');
      }
