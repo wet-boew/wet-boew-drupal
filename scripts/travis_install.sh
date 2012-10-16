0d24c7c9f929c892433523d8c6f4e3c76f421b2c
William H <sylus1984@gmail.com>
2012-10-16 11:48:13 -0300

    Update scripts/travis_install.sh

--- scripts/travis_install.sh
+++ scripts/travis_install.sh
@@ -9,15 +9,18 @@ pear channel-discover pear.drush.org
 pear install drush/drush-5.4.0
 phpenv rehash
 
-# Install WetKit Distro
-cd ..
-mkdir profiles
-mv wet-boew-drupal wetkit
-mv wetkit profiles/
-mkdir drupal_wet
-mv profiles drupal_wet/
-cd drupal_wet
-drush make --prepare-install profiles/wetkit/build-wetkit.make --yes
-sudo chmod -R 777 sites/all/modules
-drush si wetkit panopoly_theme_selection_form.theme=wetkit_adaptivetheme --sites-subdir=default --db-url=mysql://root:@127.0.0.1/wetkit_db --account-name=admin --account-pass=WetKit@2012 --site-mail=admin@example.com --site-name="Web Experience Toolkit" --yes
+# Set Up Vars
+WORKSPACE=$(pwd)
+GIT_COMMIT=$(git rev-parse HEAD)
+REPO_USER=$(git config -l | grep remote.origin.url | cut -d/ -f4)
+
+# In place edit build-wetkit.make
+sed -i "s/master/${GIT_COMMIT}/g;s/wet-boew/$REPO_USER/1" ${WORKSPACE}/build-wetkit.make
+cat ${WORKSPACE}/build-wetkit.make
+sleep 3
+
+# Drush Make
+drush make --prepare-install --no-gitinfofile --working-copy ${WORKSPACE}/build-wetkit.make ../drupal_wet --yes
+cd ../drupal_wet
+drush si wetkit wetkit_wetboew_selection_form.theme=wetkit_adaptivetheme --sites-subdir=default --db-url=mysql://root:@127.0.0.1/wetkit_db --account-name=admin --account-pass=WetKit@2012 --site-mail=admin@example.com --site-name="Web Experience Toolkit" --yes
 drush cc all --yes
