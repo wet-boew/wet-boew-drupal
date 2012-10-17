dd19aed584e8fbc9fbf0600f33181b2f3475da86
William H <sylus1984@gmail.com>
2012-10-17 17:20:00 -0300

    Update scripts/travis_install.sh

--- scripts/travis_install.sh
+++ scripts/travis_install.sh
@@ -15,14 +15,11 @@ GIT_COMMIT=$(git show --pretty=%P HEAD | head -c8)
 REPO_USER=$(git config -l | grep remote.origin.url | cut -d/ -f4)
 
 # In place edit build-wetkit.make
-ls -al
 sed -i "s/master/${GIT_COMMIT}/g;s/wet-boew/$REPO_USER/1" ${WORKSPACE}/build-wetkit.make
 cat ${WORKSPACE}/build-wetkit.make
-ls -al
-sleep 3
 
 # Drush Make
-drush make --prepare-install --no-gitinfofile --working-copy ${WORKSPACE}/build-wetkit.make ../drupal_wet --yes
+drush make --prepare-install --no-gitinfofile --working-copy ${WORKSPACE}/build-wetkit.make drupal_wet --yes
 cd ../drupal_wet
 drush si wetkit wetkit_wetboew_selection_form.theme=wetkit_adaptivetheme --sites-subdir=default --db-url=mysql://root:@127.0.0.1/wetkit_db --account-name=admin --account-pass=WetKit@2012 --site-mail=admin@example.com --site-name="Web Experience Toolkit" --yes
 drush cc all --yes
