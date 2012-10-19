3b52e661c2df19ddb289d0470dfd479ed6395db9
William H <sylus1984@gmail.com>
2012-10-19 15:22:26 -0300

    Update scripts/travis_install.sh

--- scripts/travis_install.sh
+++ scripts/travis_install.sh
@@ -19,5 +19,5 @@ mv profiles drupal_wet/
 cd drupal_wet
 drush make --prepare-install profiles/wetkit/build-wetkit.make --yes
 sudo chmod -R 777 sites/all/modules
-drush si wetkit panopoly_theme_selection_form.theme=wetkit_adaptivetheme --sites-subdir=default --db-url=mysql://root:@127.0.0.1/wetkit_db --account-name=admin --account-pass=WetKit@2012 --site-mail=admin@example.com --site-name="Web Experience Toolkit" --yes
+drush si wetkit wetkit_wetboew_selection_form.theme=wetkit_adaptivetheme --sites-subdir=default --db-url=mysql://root:@127.0.0.1/wetkit_db --account-name=admin --account-pass=WetKit@2012 --site-mail=admin@example.com --site-name="Web Experience Toolkit" --yes
 drush cc all --yes
