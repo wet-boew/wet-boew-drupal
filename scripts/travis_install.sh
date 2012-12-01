08629d35ff14403c72d75cd79170e677593dffc4
sylus <sylus@10.0.1.7>
2012-12-01 17:12:59 -0500

    Work on Build

--- scripts/travis_install.sh
+++ scripts/travis_install.sh
@@ -15,16 +15,16 @@ build_num=`git log --oneline | wc -l | tr -d ' '`
 branch_name=`git status | head -1 | sed -e 's/# On branch \(.*\)/\1/'`
 brackets_sha=`git log | head -1 | sed -e 's/commit \([0-9a-f]*$\)/\1/'`
 repo_user=`git config -l | grep remote.origin.url | cut -d/ -f4`
-GIT_COMMIT=$(git show --pretty=%P HEAD | head -c8)
+GIT_COMMIT=`git show --pretty=%P HEAD | head -c8`
+
 cat $workspace/build-wetkit.make \
 |   sed "s/\[branch\] = master/\[branch\] = $branch_name/" \
-|   sed "s/\[revision\] = master/\[revision\] = $brackets_sha/" \
+|   sed "s/\[revision\] = master/\[revision\] = $GIT_COMMIT/" \
 |   sed "s/wet-boew/sylus/1" \
 > build-wetkit.make
 
-echo ${GIT_COMMIT}
+echo $GIT_COMMIT
 echo $repo_user
-git config -l
 
 # Install WetKit Distro
 drush make -y build-wetkit.make
