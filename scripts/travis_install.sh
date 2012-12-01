81482b41b0d89439bb609224d0b455f28a4c6e2f
sylus <sylus@10.0.1.7>
2012-12-01 16:47:22 -0500

    Work on Build

--- scripts/travis_install.sh
+++ scripts/travis_install.sh
@@ -10,10 +10,10 @@ pear install drush/drush-5.4.0
 phpenv rehash
 
 # Set Up Vars
+workspace=`pwd`
 build_num=`git log --oneline | wc -l | tr -d ' '`
 branch_name=`git status | head -1 | sed -e 's/# On branch \(.*\)/\1/'`
 brackets_sha=`git log | head -1 | sed -e 's/commit \([0-9a-f]*$\)/\1/'`
-workspace=`pwd`
 repo_user=`git config -l | grep remote.origin.url | cut -d/ -f4`
 cat $workspace/build-wetkit.make \
 |   sed "s/\[branch\] = master/\[branch\] = $branch_name/" \
