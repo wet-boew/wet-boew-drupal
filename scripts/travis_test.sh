826d4b12b01507b0af54fa519adbe03f8f20bd98
sylus <sylus@10.0.1.7>
2013-01-16 21:32:07 -0500

    Let chrome do selenium testing

--- scripts/travis_test.sh
+++ scripts/travis_test.sh
@@ -19,5 +19,5 @@ DISPLAY=:99.0 ./casperjs/bin/casperjs test drupal_wet/profiles/wetkit/tests/casp
 
 # Install + Run Selenium Testing Suite
 wget http://selenium.googlecode.com/files/selenium-server-standalone-2.15.0.jar
-java -jar selenium-server-standalone-2.15.0.jar -htmlSuite "*firefox" "http://127.0.0.1:8080" "drupal_wet/profiles/wetkit/tests/selenium/WetKitTestSuite.html" "drupal_wet/profiles/wetkit/tests/selenium/Result.html"
+java -jar selenium-server-standalone-2.15.0.jar -htmlSuite "*chrome" "http://127.0.0.1:8080" "drupal_wet/profiles/wetkit/tests/selenium/WetKitTestSuite.html" "drupal_wet/profiles/wetkit/tests/selenium/Result.html"
 
