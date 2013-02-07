ff94a6c99fbf2ff72ab6a3581d65d6f6b1e8139f
William H <sylus1984@gmail.com>
2013-02-07 15:20:45 -0500

    Update scripts/travis_install.sh

--- scripts/travis_install.sh
+++ scripts/travis_install.sh
@@ -10,7 +10,6 @@ pear install drush/drush-5.8.0
 phpenv rehash
 
 # Install WetKit Distribution
-sudo chmod -R 777 /home/travis/.drush/
 workspace=`pwd`
 git_commit=`git show --pretty=%P HEAD | head -1 | cut -d\  -f 1`
 cat $workspace/build-wetkit.make | sed "s/master/$git_commit/g" | drush make --prepare-install php://stdin $workspace/build
