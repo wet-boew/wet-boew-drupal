20f08f2c8085975d68bf1edc943d60557f013b92
sylus <sylus@10.0.1.7>
2013-02-07 12:05:53 -0500

    Build Test

--- scripts/travis_test.sh
+++ scripts/travis_test.sh
@@ -4,6 +4,7 @@
 # Drush SI Drupal
 sleep 5
 cd build
+workspace=`pwd`
 drush si wetkit wetkit_wetboew_selection_form.theme=wetkit_adaptivetheme --sites-subdir=default --db-url=mysql://root:@127.0.0.1/wetkit_db --account-name=admin --account-pass=WetKit@2012 --site-mail=admin@example.com --site-name="Web Experience Toolkit" --yes
 drush cc all --yes
 
@@ -14,16 +15,16 @@ sh -e /etc/init.d/xvfb start
 sleep 3 # give xvfb some time to start
 drush runserver --server=builtin 8080 &
 sleep 3 # give xvfb some time to rebuild
-cd ../../..
+cd ..
 
 # Install + Run CasperJS Testing Suite
 git clone git://github.com/n1k0/casperjs.git
 cd casperjs
 git checkout tags/0.6.10
 cd ..
-DISPLAY=:99.0 ./casperjs/bin/casperjs test drupal_wet/profiles/wetkit/tests/casperjs/
+DISPLAY=:99.0 ./casperjs/bin/casperjs test $workspace/profiles/wetkit/tests/casperjs/
 
 # Install + Run Selenium Testing Suite
 wget http://selenium.googlecode.com/files/selenium-server-standalone-2.15.0.jar
-java -jar selenium-server-standalone-2.15.0.jar -htmlSuite "*chrome" "http://127.0.0.1:8080" "drupal_wet/profiles/wetkit/tests/selenium/WetKitTestSuite.html" "drupal_wet/profiles/wetkit/tests/selenium/Result.html"
+java -jar selenium-server-standalone-2.15.0.jar -htmlSuite "*chrome" "http://127.0.0.1:8080" "$workspace/profiles/wetkit/tests/selenium/WetKitTestSuite.html" "drupal_wet/profiles/wetkit/tests/selenium/Result.html"
 
