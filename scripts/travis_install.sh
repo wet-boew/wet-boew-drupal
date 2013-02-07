39ca758b0d1b2c0d8a0ac895462f85ee4210e2f5
William Hearn <William.Hearn@statcan.gc.ca>
2013-02-07 14:30:39 -0500

    Update Travis

--- scripts/travis_install.sh
+++ scripts/travis_install.sh
@@ -9,13 +9,8 @@ pear channel-discover pear.drush.org
 pear install drush/drush-5.8.0
 phpenv rehash
 
-# Install WetKit Distro
-cd ..
-mkdir profiles
-mv wet-boew-drupal wetkit
-mv wetkit profiles/
-mkdir drupal_wet
-mv profiles drupal_wet/
-cd drupal_wet
-drush make --prepare-install profiles/wetkit/build-wetkit.make --yes
-sudo chmod -R 777 sites/all/modules
+# Install WetKit Distribution
+sudo chmod -R 777 /home/travis/.drush/
+workspace=`pwd`
+git_commit=`git show --pretty=%P HEAD | head -1 | cut -d\  -f 1`
+cat $workspace/build-wetkit.make | sed "s/master/$git_commit/g" | drush make --prepare-install php://stdin $workspace/build
