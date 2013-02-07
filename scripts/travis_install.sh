2f3def1537bd470c53d72ee49b4bbb61a04baa36
sylus <sylus@10.0.1.7>
2013-02-07 02:37:36 -0500

    Build Test

--- scripts/travis_install.sh
+++ scripts/travis_install.sh
@@ -9,18 +9,8 @@ pear channel-discover pear.drush.org
 pear install drush/drush-5.8.0
 phpenv rehash
 
-# Set Up Variables
+# Install WetKit Distro
 workspace=`pwd`
 git_commit=`git show --pretty=%P HEAD | head -1 | cut -d\  -f 2`
 cat $workspace/build-wetkit.make | sed "s/master/$git_commit/g" | sudo drush make --prepare-install php://stdin $workspace/build
-
-# Install WetKit Distro
-#cd ..
-#mkdir profiles
-#mv wet-boew-drupal wetkit
-#mv wetkit profiles/
-#mkdir drupal_wet
-#mv profiles drupal_wet/
-#cd drupal_wet
-#drush make --prepare-install profiles/wetkit/build-wetkit.make --yes
-#sudo chmod -R 777 sites/all/modules
+cd $workspace/build
