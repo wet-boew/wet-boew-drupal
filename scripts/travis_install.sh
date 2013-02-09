b6e447ff54d56366f29c10ffadc8597f6c7098e2
William H <sylus1984@gmail.com>
2013-02-09 13:00:09 -0500

    Update scripts/travis_install.sh

--- scripts/travis_install.sh
+++ scripts/travis_install.sh
@@ -11,4 +11,4 @@ phpenv rehash
 
 # Install WetKit Distribution
 workspace=`pwd`
-cat $workspace/build-wetkit.make | sed "s/master/$TRAVIS_COMMIT/g" | sed "s/wet-boew\/wet-boew-drupal/$TRAVIS_REPO_SLUG/g" | drush make --prepare-install php://stdin $workspace/build
+cat $workspace/build-wetkit.make | sed "s@master@$TRAVIS_COMMIT@g" | sed "s@wet-boew/wet-boew-drupal@$TRAVIS_REPO_SLUG@g" | drush make --prepare-install php://stdin $workspace/build
