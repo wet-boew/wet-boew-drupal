5bf97957b19ee38d2712c1c3a338451e36e5f59a
William Hearn <sylus1984@gmail.com>
2013-02-12 16:45:55 -0500

    PHP Code Sniffer (8)

--- themes/wetkit_adaptivetheme/templates/blocks/block.tpl.php
+++ themes/wetkit_adaptivetheme/templates/blocks/block.tpl.php
@@ -17,11 +17,14 @@
  * typical navigation tempate might look like this:
  *
  * @code
- * <nav id="<?php print $block_html_id; ?>" class="<?php print $classes; ?>"<?php print $attributes; ?>>
+ * <nav id="<?php print $block_html_id; ?>" class="<?php print $classes; ?>"
+ * <?php print $attributes; ?>>
  *   <div class="block-inner clearfix">
  *     <?php print render($title_prefix); ?>
  *     <?php if ($block->subject): ?>
- *       <h2<?php print $title_attributes; ?>><?php print $block->subject; ?></h2>
+ *       <h2<?php print $title_attributes; ?>>
+ *         <?php print $block->subject; ?>
+ *       </h2>
  *     <?php endif; ?>
  *     <?php print render($title_suffix); ?>
  *     <div<?php print $content_attributes; ?>>
