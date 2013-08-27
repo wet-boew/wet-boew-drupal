6d7f6549522682669a1a6d274834de041356eb0f
Will <sylus1984@gmail.com>
2013-08-27 19:19:17 -0400

    Update Travis Scripts
    Provide some more information about what the scripts are doing.

--- scripts/travis_before_script.sh
+++ scripts/travis_before_script.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-# Travis Install Script for CI Testing
+# Travis Before Script for CI Testing
 
 # Create Database(s)
 sh -c "if [ '$DB' = 'pgsql' ]; then psql -c 'create database wetkit_db;' -U postgres; fi"
@@ -10,6 +10,6 @@ pear channel-discover pear.drush.org
 pear install drush/drush-5.8.0
 phpenv rehash
 
-# Install WetKit Distribution
+# Drush Make / Build Drupal WEM
 workspace=`pwd`
 cat $workspace/build-wetkit.make | sed "s@master@$TRAVIS_COMMIT@g" | sed "s@wet-boew/wet-boew-drupal@$TRAVIS_REPO_SLUG@g" | drush make --prepare-install php://stdin $workspace/build
