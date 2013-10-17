0d36b395d15139f2ec0eb46aaa0224a236c3852d
Will <sylus1984@gmail.com>
2013-10-17 11:11:56 -0400

    Testing beht.

--- scripts/travis_script.sh
+++ scripts/travis_script.sh
@@ -5,24 +5,26 @@
 sleep 5
 cd build
 workspace=`pwd`
-sh -c "if [ '$DB' = 'postgres' ]; then drush si wetkit wetkit_theme_selection_form.theme=wetkit_omega install_configure_form.demo_content=TRUE --sites-subdir=default --db-url=pgsql://postgres:@127.0.0.1:5432/wetkit_db --account-name=admin --account-pass=WetKit@2012 --site-mail=admin@example.com --site-name='Web Experience Toolkit' --yes; fi"
-sh -c "if [ '$DB' = 'mysql' ]; then drush si wetkit wetkit_theme_selection_form.theme=wetkit_omega install_configure_form.demo_content=TRUE --sites-subdir=default --db-url=mysql://root:@127.0.0.1:3306/wetkit_db --account-name=admin --account-pass=WetKit@2012 --site-mail=admin@example.com --site-name='Web Experience Toolkit' --yes; fi"
-drush cc all --yes
+ls -al
+pwd
+# sh -c "if [ '$DB' = 'postgres' ]; then drush si wetkit wetkit_theme_selection_form.theme=wetkit_omega install_configure_form.demo_content=TRUE --sites-subdir=default --db-url=pgsql://postgres:@127.0.0.1:5432/wetkit_db --account-name=admin --account-pass=WetKit@2012 --site-mail=admin@example.com --site-name='Web Experience Toolkit' --yes; fi"
+# sh -c "if [ '$DB' = 'mysql' ]; then drush si wetkit wetkit_theme_selection_form.theme=wetkit_omega install_configure_form.demo_content=TRUE --sites-subdir=default --db-url=mysql://root:@127.0.0.1:3306/wetkit_db --account-name=admin --account-pass=WetKit@2012 --site-mail=admin@example.com --site-name='Web Experience Toolkit' --yes; fi"
+# drush cc all --yes
 cd ..
 
 # Run composer
-cd $workspace/wet-boew-drupal/profiles/wetkit/tests/behat
-composer install
-cd $workspace
-cd ..
+# cd $workspace/wet-boew-drupal/profiles/wetkit/tests/behat
+# composer install
+# cd $workspace
+# cd ..
 
 # Headless Testing Server
-export PHANTOMJS_EXECUTABLE='phantomjs --local-to-remote-url-access=yes --ignore-ssl-errors=yes'
-export DISPLAY=:99.0
-sh -e /etc/init.d/xvfb start
-sleep 3 # give xvfb some time to start
-drush --root=$workspace/wet-boew-drupal runserver --server=builtin 8080 &
-sleep 3 # give xvfb some time to rebuild
+# export PHANTOMJS_EXECUTABLE='phantomjs --local-to-remote-url-access=yes --ignore-ssl-errors=yes'
+# export DISPLAY=:99.0
+# sh -e /etc/init.d/xvfb start
+# sleep 3 # give xvfb some time to start
+# drush --root=$workspace/wet-boew-drupal runserver --server=builtin 8080 &
+# sleep 3 # give xvfb some time to rebuild
 
 # Install + Run CasperJS Testing Suite
 # git clone git://github.com/n1k0/casperjs.git
@@ -32,13 +34,13 @@ sleep 3 # give xvfb some time to rebuild
 # DISPLAY=:99.0 ./casperjs/bin/casperjs test $workspace/profiles/wetkit/tests/casperjs/
 
 # Install + Run Selenium Testing Suite
-wget http://selenium.googlecode.com/files/selenium-server-standalone-2.25.0.jar
-java -jar selenium-server-standalone-2.25.0.jar -p 4444 &
+# wget http://selenium.googlecode.com/files/selenium-server-standalone-2.25.0.jar
+# java -jar selenium-server-standalone-2.25.0.jar -p 4444 &
 # wget http://selenium.googlecode.com/files/selenium-server-standalone-2.15.0.jar
 # java -jar selenium-server-standalone-2.15.0.jar -htmlSuite "*chrome" "http://127.0.0.1:8080" "$workspace/profiles/wetkit/tests/selenium/WetKitTestSuite.html" "$workspace/profiles/wetkit/tests/selenium/Result.html"
-sleep 5
+# sleep 5
 
-cd $workspace/wet-boew-drupal/profiles/wetkit/tests/behat
-touch behat.local.yml
-ls
-./bin/behat
+# cd $workspace/wet-boew-drupal/profiles/wetkit/tests/behat
+# touch behat.local.yml
+# ls
+# ./bin/behat
