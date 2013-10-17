a844a28b856ffa9b1d11e824923ca3b5c02db469
Will <sylus1984@gmail.com>
2013-10-16 22:42:00 -0400

    dir

--- scripts/travis_script.sh
+++ scripts/travis_script.sh
@@ -29,5 +29,5 @@ DISPLAY=:99.0 ./casperjs/bin/casperjs test $workspace/profiles/wetkit/tests/casp
 wget http://selenium.googlecode.com/files/selenium-server-standalone-2.25.0.jar
 java -jar selenium-server-standalone-2.25.0.jar -p 4444 &
 sleep 5
-
+cd /tests/behat
 ls
