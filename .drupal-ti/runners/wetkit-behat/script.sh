de67c6a0d934edd211860780f9365d354af0c553
William Hearn <sylus1984@gmail.com>
2015-09-09 20:48:09 -0400

    Update testing infra to align with DRUPAL_TI
    From the brilliant folks at: https://github.com/LionsAd/drupal_ti

---
+++ .drupal-ti/runners/wetkit-behat/script.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+# @file
+# Behat integration - Script step.
+#
+## Template modified from http://drupal.org/project/panopoly
+
+set -e $DRUPAL_TI_DEBUG
+
+# Ensure we are in the right directory.
+cd "$DRUPAL_TI_DRUPAL_DIR"
+
+# Now go to the local behat tests, being within the module installation is
+# needed for example for the drush runner.
+cd "$DRUPAL_TI_BEHAT_DIR"
+
+wetkit_header Running tests
+
+# Copy into place because it doesn't come with wetkit_test.
+mv -f "$TRAVIS_BUILD_DIR"/behat.travis.yml.dist .
+
+# If this isn't an upgrade, we test if any features are overridden.
+if [[ "$UPGRADE" == none ]]
+then
+	"$TRAVIS_BUILD_DIR"/scripts/check-overridden.sh
+fi
+
+# This replaces environment vars from $DRUPAL_TI_BEHAT_YML into 'behat.yml'.
+drupal_ti_replace_behat_vars
+
+ARGS=( $DRUPAL_TI_BEHAT_ARGS )
+
+# First, run all the tests in Firefox.
+./bin/behat "${ARGS[@]}"
+
+# Then run some Chrome-only tests.
+./bin/behat -p chrome "${ARGS[@]}"
